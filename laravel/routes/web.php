53525e8d4a6b9f252138de37be7a21f1af1c5b85
sa-borgia3 <sa.borgia3@gmail.com>
2021-03-02 19:42:36 +0100

    sistemazione home welcome

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -11,10 +11,10 @@ Route::get('/dashboard', 'HomeController@dashboard')->name('dashboard');
 Route::get('/', 'MainController@index')->name('index');
 
 // route per la ricerca in homepage
-Route::post('/search/{id}', 'MainController@search') -> name('search');
+Route::post('/search/{id}', 'MainController@search')->name('search');
 
 // route show ristorante
-Route::get('/restaurant/show/{id}', 'MainController@restaurantShow') -> name('show-restaurant');
+Route::get('/restaurant/show/{id}', 'MainController@restaurantShow')->name('show-restaurant');
 
 // Route USERS
 Route::resource('users', 'UserController');
