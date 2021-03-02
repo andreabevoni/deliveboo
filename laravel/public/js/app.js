a67ca9b3acb3902f91811e579a4a3fd033959b0e
Caterina <danizio@outlook.it>
2021-03-02 17:36:47 +0100

    Fix

--- laravel/public/js/app.js
+++ laravel/public/js/app.js
@@ -49995,8 +49995,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/salwo92/Documenti/progettoFinale/deliveboo/laravel/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/salwo92/Documenti/progettoFinale/deliveboo/laravel/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Laravel\deliveboo\laravel\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Laravel\deliveboo\laravel\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
