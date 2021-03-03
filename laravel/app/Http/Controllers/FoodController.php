2dbbbae7409f7bce7b05c25d9abdf654b5139bd5
sa-borgia3 <sa.borgia3@gmail.com>
2021-03-03 14:57:56 +0100

    create and store food

--- laravel/app/Http/Controllers/FoodController.php
+++ laravel/app/Http/Controllers/FoodController.php
@@ -5,30 +5,49 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Food;
+use Illuminate\Support\Facades\Auth;
 
 class FoodController extends Controller
 {
     // index
-    public function index() {
-        // $userAuth = Auth::user();
+    public function index()
+    {
+        $userAuth = Auth::user();
+        $id = $userAuth->id;
 
-        $foods = Food::all();
+        $foods = Food::orderBy('id', 'DESC')->where(function ($query) use ($id) {
+            $query->where('user_id', '=', $id);
+        })->get();
+        // dd($foods);
+
 
         // dd($food);
         return view('foods-page', compact('foods'));
     }
 
     // create
-    public function create() {
+    public function create()
+    {
         $foods = Food::all();
         return view('food-create', compact('foods'));
     }
 
     // store
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
 
-        dd($request -> all());
+        // dd($request -> all());
 
-        return redirect() -> route('index-food');
+        $data = $request->all();
+        $user = Auth::user();
+
+        $food = Food::make($data);
+
+        $food->user()->associate($user);
+        // dd($food);
+
+        $food->save();
+
+        return redirect()->route('food.index');
     }
 }
