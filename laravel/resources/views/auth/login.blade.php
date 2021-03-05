7f547e539c0fa8d3233a36bd670321c143f66c05
Caterina <danizio@outlook.it>
2021-03-05 17:43:38 +0100

    Style login page + mixins creation

--- laravel/resources/views/auth/login.blade.php
+++ laravel/resources/views/auth/login.blade.php
@@ -5,14 +5,16 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div>
-                <div>{{ __('Login') }}</div>
+                <div class="login-form">
+                  <h1>{{ __('Accedi alla tua area riservata') }}</h1>
+                </div>
 
                 <div>
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Indirizzo email') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
@@ -39,30 +41,35 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                        <div class="login-form form-group row">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                     <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
+                                        {{ __('Ricordati di me') }}
                                     </label>
                                 </div>
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
+                        <div class="login-form form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
+                                <button type="submit">
+                                    {{ __('Continua') }}
                                 </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
                             </div>
+
+                            @if (Route::has('password.request'))
+                            <div class="col-md-8 offset-md-4">
+                              <button class="white" type="button" name="button">
+                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                  {{ __('Password dimenticata?') }}
+                                </a>
+                              </button>
+                            </div>
+
+                            @endif
                         </div>
                     </form>
                 </div>
