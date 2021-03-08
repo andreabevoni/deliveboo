d2156d755acf1d42b4448961ea6dea6282598987
Andrea Bevoni <andreabevoni@gmail.com>
2021-03-08 18:22:48 +0100

    Update 2.0 componente checkout

--- laravel/resources/views/pages/checkout.blade.php
+++ laravel/resources/views/pages/checkout.blade.php
@@ -21,5 +21,30 @@
       :user_id="'{{$user -> id}}'"
     ></checkout>
 
+    <div id="dropin-container"></div>
+
 </div>
 @endsection
+
+{{-- @php
+  $path = base_path();
+  require_once $path . '/braintree/lib/Braintree.php';
+  $gateway = new \Braintree\Gateway([
+    'environment' => 'sandbox',
+    'merchantId' => 'hgvcd7wdfvcgzx7h',
+    'publicKey' => 'pnx3pfrwprvcnhxd',
+    'privateKey' => 'cd2d8ffc57f426d67fc3af282a183dd5'
+  ]);
+  $clientToken = $gateway->clientToken()->generate();
+@endphp --}}
+
+{{-- <script src="https://js.braintreegateway.com/web/dropin/1.26.1/js/dropin.min.js"></script> --}}
+
+{{-- <script type="text/javascript">
+  braintree.dropin.create({
+    container: document.getElementById('dropin-container'),
+    authorization: this.clientToken,
+    container: '#dropin-container'
+  }, (error, dropinInstance) => {
+  });
+</script> --}}
