c8fdbb3be401034b1d47150dafbdb6426c595795
sa-borgia3 <sa.borgia3@gmail.com>
2021-03-11 17:19:55 +0100

    bugfix ripristina food

--- laravel/resources/views/pages/food-restore.blade.php
+++ laravel/resources/views/pages/food-restore.blade.php
@@ -4,18 +4,17 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                
+
                 <h2 class="mx-5 mt-5 mb-3">{{ __('Ripristina Piatto') }}</h2>
                 <div class="cartella mb-5 p-5">
 
-                    <form method="POST" action="{{ route('restore-task') }}">
+                    <form method="POST" action="{{ route('restore-food') }}">
                         @csrf
                         @method('POST')
 
                         <div class="d-flex flex-wrap justify-content-between">
                             <div class="col-sm-10">
-                                <label for="name"
-                                    class="p-3">{{ __('Nome Piatto') }}</label>
+                                <label for="name" class="p-3">{{ __('Nome Piatto') }}</label>
 
                                 <select name="name" id="">
                                     @foreach ($deletedFood as $food)
@@ -24,13 +23,13 @@
                                     @endforeach
                                 </select>
                             </div>
-                    
+
                             <div class="">
                                 <button type="submit" class="bottone-generale">
                                     {{ __('Ripristina') }}
                                 </button>
                             </div>
-                        
+
                         </div>
 
                     </form>
