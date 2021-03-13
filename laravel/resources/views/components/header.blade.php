52ed973c3c959c8e8a8c2cbac0b33dcf2c8d8f96
Davide <kalelc351k@gmail.com>
2021-03-13 15:40:27 +0100

    fix dropdown

--- laravel/resources/views/components/header.blade.php
+++ laravel/resources/views/components/header.blade.php
@@ -1,5 +1,5 @@
 <header>
-    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+    <nav class="navbar navbar-expand-sm navbar-light bg-white shadow-sm">
       <div class="container">
 
         {{-- Left menu --}}
